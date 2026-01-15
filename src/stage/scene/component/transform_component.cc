7d941c2d16bf047a997ff05e3d327a98b54044aa
VloRyan <florian685@gmail.com>
2026-01-15 14:58:37 +0100

    feat: SetLocalPosition sets the value immediately

--- src/stage/scene/component/transform_component.cc
+++ src/stage/scene/component/transform_component.cc
@@ -11,47 +11,55 @@ void TransformComponent::UpdateTransform(const glm::mat4& parentTransform) {
 }
 
 void TransformComponent::SetPosition(const glm::vec3& pos) {
-  auto relPos =
-      pos - worldTransform_.GetPosition() + localTransform_.GetPosition();
-  if (relPos == localTransform_.GetPosition()) {
+  if (pos == GetPosition()) {
     return;
   }
-  localTransform_.SetPosition(relPos);
+  auto parentPos =
+      worldTransform_.GetPosition() - localTransform_.GetPosition();
+  localTransform_.SetPosition(pos - parentPos);
   worldTransform_.SetPosition(pos);
-  SignalChanged();
+  SignalChanged(-1);
 }
 
 glm::vec3 TransformComponent::GetPosition() const {
   return worldTransform_.GetPosition();
 }
+
 void TransformComponent::SetLocalPosition(const glm::vec3& pos) {
-  auto diff = pos - localTransform_.GetPosition();
-  if (diff == glm::vec3(0.F)) {
+  if (pos == GetLocalPosition()) {
     return;
   }
+  auto parentPos =
+      worldTransform_.GetPosition() - localTransform_.GetPosition();
   localTransform_.SetPosition(pos);
-  worldTransform_.SetPosition(worldTransform_.GetPosition() + diff);
-  SignalChanged();
+  worldTransform_.SetPosition(parentPos + pos);
+  SignalChanged(-1);
 }
+
 glm::vec3 TransformComponent::GetLocalPosition() const {
   return localTransform_.GetPosition();
 }
+
 glm::vec3 TransformComponent::GetDirection() const {
   return worldTransform_.GetDirection();
 }
 
 glm::vec3 TransformComponent::GetUp() const { return worldTransform_.GetUp(); }
+
 glm::vec3 TransformComponent::GetRight() const {
   return worldTransform_.GetRight();
 }
+
 const glm::mat4& TransformComponent::GetMatrix() const {
   return worldTransform_.GetMatrix();
 }
+
 const glm::mat4& TransformComponent::GetLocalMatrix() const {
   return localTransform_.GetMatrix();
 }
+
 void TransformComponent::SetLocalMatrix(const glm::mat4& transform) {
   localTransform_.SetMatrix(transform);
-  SignalChanged();
+  SignalChanged(-1);
 }
 };  // namespace soil::stage::scene::component
